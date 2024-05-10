9265ea66463634701d22361d8e11b02472288297
artur-chaikouski <artur.chaikouski@smartapptech.net>
2024-05-10 19:13:30 +0300

    add footer
    add footer

---
+++ src/js/footer-script/form-mobile-validation.js
@@ -0,0 +1,99 @@
+import { validateEmail } from '../utils/form-validation';
+
+const formElement = document.getElementById('ContactFooterMobile');
+const inputElement = formElement.querySelector('input.newsletter__input_mobile');
+const sendFormButtonElement = formElement.querySelector('#SubscribeMobile');
+const footerErrorMessageContainerElement = document.getElementById('FooterErrorMessageContainerMobile');
+const errorInputClassName = 'footerFormSideInput_error';
+
+const updateValidationMessage = (status) => {
+  const emptyErrorMessageElement = footerErrorMessageContainerElement.querySelector(`[data-error-type="empty"]`);
+  const invalidErrorMessageElement = footerErrorMessageContainerElement.querySelector(`[data-error-type="invalid"]`);
+
+  switch (status) {
+    case 'empty': {
+      if (!inputElement.classList.contains(errorInputClassName)) {
+        inputElement.classList.add(errorInputClassName);
+      }
+
+      if (emptyErrorMessageElement.classList.contains('disp-none-imp')) {
+        emptyErrorMessageElement.classList.remove('disp-none-imp');
+      }
+
+      if (!invalidErrorMessageElement.classList.contains('disp-none-imp')) {
+        invalidErrorMessageElement.classList.add('disp-none-imp');
+      }
+      break;
+    }
+    case 'invalid': {
+      if (!inputElement.classList.contains(errorInputClassName)) {
+        inputElement.classList.add(errorInputClassName);
+      }
+
+      if (invalidErrorMessageElement.classList.contains('disp-none-imp')) {
+        invalidErrorMessageElement.classList.remove('disp-none-imp');
+      }
+
+      if (!emptyErrorMessageElement.classList.contains('disp-none-imp')) {
+        emptyErrorMessageElement.classList.add('disp-none-imp');
+      }
+      break;
+    }
+    case 'hide': {
+      if (inputElement.classList.contains(errorInputClassName)) {
+        inputElement.classList.remove(errorInputClassName);
+      }
+
+      if (!invalidErrorMessageElement.classList.contains('disp-none-imp')) {
+        invalidErrorMessageElement.classList.add('disp-none-imp');
+      }
+
+      if (!emptyErrorMessageElement.classList.contains('disp-none-imp')) {
+        emptyErrorMessageElement.classList.add('disp-none-imp');
+      }
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+if (inputElement) {
+  inputElement.addEventListener('input', (event) => {
+    const isInputEmpty = !inputElement.value;
+
+    if (inputElement && isInputEmpty) {
+      formElement.setAttribute('data-empty', '');
+    } else if (inputElement) {
+      formElement.removeAttribute('data-empty');
+    }
+
+    if (footerErrorMessageContainerElement) {
+      updateValidationMessage('hide');
+    }
+  });
+}
+
+if (sendFormButtonElement) {
+  sendFormButtonElement.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const isEmptyInputValue = formElement.hasAttribute('data-empty');
+
+    if (isEmptyInputValue && footerErrorMessageContainerElement) {
+      updateValidationMessage('empty');
+
+      return;
+    } else {
+      const isEmailValueValid = validateEmail(inputElement.value);
+
+      if (!isEmailValueValid && footerErrorMessageContainerElement) {
+        updateValidationMessage('invalid');
+
+        return;
+      }
+    }
+
+    formElement.submit();
+  });
+}
---
+++ src/js/footer-script/index.js
@@ -0,0 +1,3 @@
+import './mobile-dropdowns';
+import './form-mobile-validation';
+import './form-desktop-validation';
