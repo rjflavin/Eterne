c104b934dbd3b3a90f272889b40dd2ace85b0fae
artur-chaikouski <artur.chaikouski@smartapptech.net>
2024-05-27 15:30:47 +0300

    add filters logic

--- src/js/collection-script/index.js
+++ src/js/collection-script/index.js
@@ -1,3 +1,13 @@
 import { EterneCollection } from "./eterneCollection";
+import { FiltersForm } from './filtersForm';
 
 if (!customElements.get('eterne-collection')) customElements.define('eterne-collection', EterneCollection);
+
+if (!customElements.get('collection-filters-form')) {
+  customElements.define('collection-filters-form', FiltersForm);
+
+  FiltersForm.filterData = [];
+  FiltersForm.searchParamsInitial = window.location.search.slice(1);
+  FiltersForm.searchParamsPrev = window.location.search.slice(1);
+  FiltersForm.setListeners();
+}
