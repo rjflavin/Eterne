282d649fdc91c844d4a2d70919626a101429633b
artur-chaikouski <artur.chaikouski@smartapptech.net>
2024-06-20 15:46:42 +0300

    fix header z-index when it's hidden

--- src/js/header-script/hide-on-scroll.js
+++ src/js/header-script/hide-on-scroll.js
@@ -1,6 +1,7 @@
 let oldValue = window.pageYOffset || document.documentElement.scrollTop;
 let newValue = window.pageYOffset || document.documentElement.scrollTop;
 const headerElement = document.querySelector('page-header');
+const rootHeaderElement = document.querySelector('.section-header');
 
 window.addEventListener('scroll', () => {
     // Get the new Value
@@ -13,12 +14,14 @@ window.addEventListener('scroll', () => {
         headerElement.style.opacity = 0;
         headerElement.style.visibility = 'hidden';
         headerElement.style.zIndex = '-1';
+        rootHeaderElement.style.zIndex = '0';
 
     } else if(oldValue - newValue > 0){
 
         headerElement.style.opacity = 1;
         headerElement.style.visibility = 'visible';
         headerElement.style.zIndex = '999';
+        rootHeaderElement.style.zIndex = '999';
     }
 
     // Update the old value
