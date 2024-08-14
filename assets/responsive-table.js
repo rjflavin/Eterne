442872e904a9ddf641ab40cff9faa13e5fc8baa0
artur-chaikouski <artur.chaikouski@smartapptech.net>
2024-08-14 12:33:39 +0300

    minify css and js, fix scripts for theme.liquid

--- assets/responsive-table.js
+++ assets/responsive-table.js
@@ -1,17 +1,1 @@
-if (!customElements.get('responsive-table')) {
-  class ResponsiveTable extends HTMLElement {
-    connectedCallback() {
-      this.querySelector('.responsive-table').addEventListener('click', ResponsiveTable.handleClick);
-    }
-
-    static handleClick(evt) {
-      const head = evt.target.closest('.responsive-table__cell-head');
-      if (head && getComputedStyle(head.closest('.responsive-table')).display === 'block') {
-        evt.preventDefault();
-        head.closest('tr').classList.toggle('expanded');
-      }
-    }
-  }
-
-  customElements.define('responsive-table', ResponsiveTable);
-}
+if(!customElements.get("responsive-table")){class e extends HTMLElement{connectedCallback(){this.querySelector(".responsive-table").addEventListener("click",e.handleClick)}static handleClick(e){let s=e.target.closest(".responsive-table__cell-head");s&&"block"===getComputedStyle(s.closest(".responsive-table")).display&&(e.preventDefault(),s.closest("tr").classList.toggle("expanded"))}}customElements.define("responsive-table",e)}
